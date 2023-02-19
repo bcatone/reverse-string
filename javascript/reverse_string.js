11dc6ebfc8ee52acc7ad9b9aed0fc155b538a331
Brittany Laura Catone <bcatone@outlook.com>
2023-02-18 21:12:56 -0500

    commit tree b441734b1f7db21acd7599680452281ac6bee03b

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,13 @@
 function reverseString(str) {
   // type your code here
+  let reversedString = "";
+  let length = str.length;
+
+  for (let i = length; i >= 0; i--) {
+    reverseString = str[i];
+  };
+
+  return reverseString;
 }
 
 if (require.main === module) {
@@ -17,3 +25,10 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// 1. Create an empty string
+// 2. Get the length of the string
+// 3. Set i to one less than the length of the string in a for-loop
+// 4. Get the char at that index
+// 5. Concatenate the char to the string
+// 6. Decrement i by 1
